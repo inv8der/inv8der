281685f62ae9f1467ce844ef96b43242b48ab08e
Marc Ryan <ryanmq87@gmail.com>
2014-10-14 09:35:56 -0700

    Navigating to the index page will now redirect you to my new and improved Randomizer app! Also added two endpoints for cracking an md5 hash.

--- index.js
+++ index.js
@@ -1,123 +1,57 @@
-var Hapi = require("hapi");
-var BeatsRandomizer = require("./lib/randomizer");
-
-var randomizer = new BeatsRandomizer({client_id: "38q2wmxsng5k7gdbj5vyq8t6"});
-
-var config = {
-    urls: {
-        failureRedirect: "/login",
-        successRedirect: "/"
-    },
-    beatsmusic: {
-        clientID: "38q2wmxsng5k7gdbj5vyq8t6",
-        clientSecret: "bjXb8gxAuSUFbeF2wuPWG9dx",
-        callbackURL: "http://inv8der.herokuapp.com/auth/beatsmusic/callback"
-    }
-};
-
-var plugins = {
-    "yar": {
-        cookieOptions: {
-            password: "cookiemonster",
-            isSecure: false
-        }
-    },
-    "./plugins/beats-authenticator": config
-};
+var Hapi = require('hapi');
+var Wreck = require('wreck');
 
 var port = Number(process.env.PORT || 5000);
-var server = new Hapi.Server(port, {
-	views: {
-        engines: { html: "handlebars" },
-        path: "./views"
-	}
-});
+var server = new Hapi.Server(port, {cors: true});
 
-server.pack.require(plugins, function(err) {
-	if (err) throw err;
-});
-
-// Registers the authentication strategy to use
-server.auth.strategy("passport", "passport");
-
-server.route({
-	path: "/",
-	method: "GET",
-	config: { 
-		auth: "passport",
-		handler: function(request, reply) {
-			var user_id = request.session.user.profile.id;
-			var accessToken = request.session.user.accessToken;
-
-			//randomizer.createSession(request.session);
-
-			randomizer.getPlaylist(user_id, accessToken, function(playlist_id) {
-				console.log(playlist_id);
-			});
-
-			reply.view("index", {
-				clientId: "38q2wmxsng5k7gdbj5vyq8t6",
-				accessToken: accessToken,
-				userId: user_id
-			});
-		}
-	}
-});
+var api = {
+    md5crack: { 
+        key: '3310f3cf0b86b9103f05dae7',
+        baseURL: 'http://api.md5crack.com/'
+    },
+    md5online: {
+        key: '7788205158896745',
+        baseURL: 'http://www.md5online.org/api.php'
+    }
+}
 
 server.route({
-	path: "/static/{path*}",
-	method: "GET",
-	handler: {
-		directory: {
-			path: "./public",
-			listing: false,
-			index: false
-		}
-	}
-});
-
-server.route({
-	path: "/beats-randomizer/randomize",
-	method: "GET",
-	handler: function(request, reply) {
-		var user_id = request.session.user.profile.id;
-        randomizer.randomize(user_id, function(json) {
-        	reply(json);
-        });
-	}
-});
-
-
-server.route({
-	path: "/beats-randomizer/like/{id}",
-	method: "GET",
+	path: '/',
+	method: 'GET',
 	config: {
-		auth: "passport",
+        cors: false,
 		handler: function(request, reply) {
-			var user_id = request.session.user.profile.id;
-			var accessToken = request.session.user.accessToken;
-			var track_id = request.params.id
-			randomizer.addItem(user_id, track_id, accessToken, function(json) {
-				reply(json);
-			});
+			reply.redirect('http://randomizr.herokuapp.com/');
 		}
 	}
 });
 
 server.route({
-	path: "/beats-randomizer/dislike/{id}",
-	method: "GET",
+	path: '/md5crack/{hash}',
+	method: 'GET',
 	config: {
-		auth: "passport",
 		handler: function(request, reply) {
-			var user_id = request.session.user.profile.id;
-			var accessToken = request.session.user.accessToken;
-			var track_id = request.params.id
-			randomizer.removeItem(user_id, track_id, accessToken, function(json) {
-				reply(json);
-			});
+            var url = api.md5crack.baseURL + '/crack/' + api.md5crack.key + '/' + request.params.hash
+			Wreck.get(url, {json: true}, function(err, response, payload) {
+                if (err) throw err;
+                reply(payload);
+            });
 		}
 	}
+});
+
+server.route({
+    path: '/md5decrypt/{hash}',
+    method: 'GET',
+    config: {
+        handler: function(request, reply) {
+            var url = api.md5online.baseURL + '?p=' + api.md5online.key + '&h=' + request.params.hash
+            Wreck.get(url, function(err, response, payload) {
+                if (err) throw err;
+                reply(payload);
+            });
+        }
+    }
 });
 
 server.start(function () {
